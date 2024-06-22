b2533cdb95ecc0a7e03ce2080e91d70297b58fd3
phuctq32 <phuctq32@gmail.com>
2024-06-22 15:13:04 +0700

    feat: add unit tests for ComputePath util

--- test_suite/utils_test.go
+++ test_suite/utils_test.go
@@ -1,8 +1,54 @@
 package testsuite
 
-import "testing"
+import (
+	"nerdlp/engine.go"
+	"testing"
+)
 
 // TODO: need to write test for this
 func TestComputePath(t *testing.T) {
-	t.Skip()
+	tests := map[string]struct {
+		input          string
+		handleResponse func(t *testing.T, out string)
+	}{
+		"no_leading_slash": {
+			input: "engine.io",
+			handleResponse: func(t *testing.T, out string) {
+				if out != "/engine.io/" {
+					t.Fatalf("expected \\engine.io\\, but got %s", out)
+				}
+			},
+		},
+		"leading_slashes": {
+			input: "///engine.io",
+			handleResponse: func(t *testing.T, out string) {
+				if out != "/engine.io/" {
+					t.Fatalf("expected \\engine.io\\, but got %s", out)
+				}
+			},
+		},
+		"trailing_slashes": {
+			input: "engine.io////",
+			handleResponse: func(t *testing.T, out string) {
+				if out != "/engine.io/" {
+					t.Fatalf("expected \\engine.io\\, but got %s", out)
+				}
+			},
+		},
+		"leading_slashes_and_trailing_slashes": {
+			input: "///engine.io//",
+			handleResponse: func(t *testing.T, out string) {
+				if out != "/engine.io/" {
+					t.Fatalf("expected \\engine.io\\, but got %s", out)
+				}
+			},
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			output := engine.ComputePath(tt.input)
+			tt.handleResponse(t, output)
+		})
+	}
 }
